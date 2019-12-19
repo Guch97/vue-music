b8330b6f7267d9d3c253ad369cf8c4c83aac62e1
gchgch789 <930571051@qq.com>
2019-12-19 21:42:30 +0800

    first init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+      <m-header></m-header>
+  </div>
+</template>
+
+<script>
+import MHeader from '../src/components/m-header/m-header'
+export default {
+componets:{
+    MHeader
+}
+
+}
+</script>
+
+<style scoped lang=stylus rel="stylesheet/stylus">
+
+</style>
