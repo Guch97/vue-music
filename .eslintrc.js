278db17f88483861f3474b816d6e92e3791de6d1
gchgch789 <930571051@qq.com>
2020-02-28 12:01:03 +0800

    fix babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    // 'eslint:recommended'
-  ],
-  // 'rules': {
-  //   // allow paren-less arrow functions
-  //   'arrow-parens': 0,
-  //   // allow async-await
-  //   'generator-star-spacing': 0,
-  //   // allow debugger during development
-  //   'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-  //   'eol-last': 0,
-  //   'space-before-function-paren': 0
-  // },
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        '@vue/standard'
+        // 'eslint:recommended'
+    ],
+    // 'rules': {
+    //   // allow paren-less arrow functions
+    //   'arrow-parens': 0,
+    //   // allow async-await
+    //   'generator-star-spacing': 0,
+    //   // allow debugger during development
+    //   'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    //   'eol-last': 0,
+    //   'space-before-function-paren': 0
+    // },
 
-  // parserOptions: {
-  //   parser: 'babel-eslint'
-  // }
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,59 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Recommend from 'views/recommend/recommend.vue';
-import Singer from 'views/singer/singer.vue';
-import Rank from 'views/rank/rank.vue';
-import Search from 'views/search/search.vue';
-import SingerDetail from 'views/singer-detail/singer-detail.vue'
-import Disc from 'views/recommend-disc/disc.vue'
-import rankTopList from 'views/rank-top-list/rank-top-list.vue'
-import UserCenter from 'views/user-center/user-center.vue'
-
-
-
+// import Recommend from 'views/recommend/recommend.vue';
+// import Singer from 'views/singer/singer.vue';
+// import Rank from 'views/rank/rank.vue';
+// import Search from 'views/search/search.vue';
+// import SingerDetail from 'views/singer-detail/singer-detail.vue'
+// import Disc from 'views/recommend-disc/disc.vue'
+// import rankTopList from 'views/rank-top-list/rank-top-list.vue'
+// import UserCenter from 'views/user-center/user-center.vue'
 
 Vue.use(VueRouter)
+
+const Recommend = ((resolve) => {
+    import ('views/recommend/recommend.vue').then((module) => {
+        resolve(module)
+    })
+})
+const Singer = ((resolve) => {
+    import ('views/singer/singer.vue').then((module) => {
+        resolve(module)
+    })
+})
+const Rank = ((resolve) => {
+    import ('views/rank/rank.vue').then((module) => {
+        resolve(module)
+    })
+})
+const Search = ((resolve) => {
+    import ('views/search/search.vue').then((module) => {
+        resolve(module)
+    })
+})
+const SingerDetail = ((resolve) => {
+    import ('views/singer-detail/singer-detail.vue').then((module) => {
+        resolve(module)
+    })
+})
+const Disc = ((resolve) => {
+    import ('views/recommend-disc/disc.vue').then((module) => {
+        resolve(module)
+    })
+})
+const rankTopList = ((resolve) => {
+    import ('views/rank-top-list/rank-top-list.vue').then((module) => {
+        resolve(module)
+    })
+})
+const UserCenter = ((resolve) => {
+    import ('views/user-center/user-center.vue').then((module) => {
+        resolve(module)
+    })
+})
+
+
+
 
 const routes = [{
     path: '/',
